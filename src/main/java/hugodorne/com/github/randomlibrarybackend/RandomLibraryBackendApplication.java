24359e09d24ffd8600a13f7bb55a495a5d004212
Hugo Dorne <hugodorne@gmail.com>
2022-10-16 21:08:18 +0200

    Init SpringBoot Project

---
+++ src/main/java/hugodorne/com/github/randomlibrarybackend/RandomLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package hugodorne.com.github.randomlibrarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hugodorne/com/github/randomlibrarybackend/RandomLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hugodorne.com.github.randomlibrarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
